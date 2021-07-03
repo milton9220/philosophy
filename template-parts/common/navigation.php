4e8de0c7983d6e5bf3057c1fae04ac8caa7e0d17
milton9220 <monjurhasan105142@gmail.com>
2021-07-04 01:44:04 +0600

    added custom class in menu submenu with php code

--- template-parts/common/navigation.php
+++ template-parts/common/navigation.php
@@ -6,14 +6,16 @@
 
                     <?php 
                     
-                        wp_nav_menu(array(
+                       $philosophy_menu= wp_nav_menu(array(
 
                             'theme_location'    =>"topmenu",
                             "menu_id"           =>"topmenu",
-                            'menu_class'        =>"header__nav"
+                            'menu_class'        =>"header__nav",
+                            "echo"              =>false
 
                         ));
-                    
+                        $philosophy_menu=str_replace("menu-item-has-children","menu-item-has-children has-children",$philosophy_menu);
+                        echo $philosophy_menu;
                     ?>
 
 
